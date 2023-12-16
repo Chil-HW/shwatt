2ca03242929df2f2dc3b4dd094167d7a395873e0
Karl Hallsby <karl@hallsby.com>
2023-12-15 20:22:40 -0600

    Use log4cl in default hello
    This was used for quick testing of log4cl's behavior.

--- source/shwatt.lisp
+++ source/shwatt.lisp
@@ -1,8 +1,11 @@
 (defpackage shwatt
-  (:use :cl)
+  (:use :cl :log4cl)
   (:export #:hello))
 
 (in-package :shwatt)
 
 (defun hello ()
-  (format t "Hello to SHWaTT!~&"))
+  (let ((msg (format 'nil "Hello to SHWaTT!")))
+    (log:info msg)
+    (when (log:debug)
+      (log:debug "This is a fancy debugger!"))))
