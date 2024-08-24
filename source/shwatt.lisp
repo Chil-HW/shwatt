ab1dc77d560d79c383628b32bf3cbebc969f6a76
Karl Hallsby <karl@hallsby.com>
2024-08-24 12:22:24 -0500

    Put toy skeleton in place for running the simulator

--- source/shwatt.lisp
+++ source/shwatt.lisp
@@ -4,8 +4,23 @@
 
 (in-package :shwatt)
 
+(defun handle-ir ()
+  "Read the IR and convert to simulator's internal representation of the circuit,
+which is amenable to simulation."
+  (log:trace "Starting to handle IR")
+  nil)
+
+(defun simulate ()
+  "Simulate the provided circuit."
+  (log:trace "Simulation starting")
+  nil)
+
 (defun hello ()
-  (let ((msg (format 'nil "Hello to SHWaTT!")))
-    (log:info msg)
-    (when (log:debug)
-      (log:debug "This is a fancy debugger!"))))
+  (log:config :trace)
+  (progn
+    (let ((msg (format 'nil "Hello to SHWaTT!")))
+      (log:info msg)
+      (when (log:debug)
+        (log:debug "This is a fancy debugger!")))
+    (handle-ir)
+    (simulate)))
